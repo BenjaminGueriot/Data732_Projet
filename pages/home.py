074332014f1115287dbd4897f181406e5bcdfe1b
BenjaminGueriot <90306512+BenjaminGueriot@users.noreply.github.com>
2022-12-12 14:31:25 +0100

    update home.py

--- pages/home.py
+++ pages/home.py
@@ -1,5 +1,5 @@
 import dash
-from dash import html, dcc
+from dash import html
 import base64
 
 dash.register_page(__name__, path='/')
@@ -11,7 +11,7 @@ Introduction_text = [
      ]
 
 Countries_text = [
-    "Ce tableau de bord est centré sur l'analyse des pays. En effet, nous plaçons sur une carte du monde, un marqueur représentant le nombre de fois où ce pays a été cité dans les articles du fichier sélectionné ", 
+    "Ce tableau de bord est centré sur l'analyse des pays. En effet, nous plaçons sur une carte du monde, un marqueur représentant le nombre de fois où ce pays a été cité dans les articles du fichier sélectionné.", 
     html.Br(), 
     "Lorsque nous cliquons sur celui-ci, nous affichons un graphique en barres permettant de visualiser les 5 dates où ce pays a été cité le plus de fois pour un article ( 5 dates maximum). En cliquant sur une barre, nous allons afficher le titre ainsi que la description de l’article en question.", 
     html.Br(), 
@@ -21,9 +21,9 @@ Countries_text = [
     ]
 
 Keywords_text = [
-    "Ce tableau de bord est focalisé sur les mot clés. Abordons d’abord le graphique principal. Nous pouvons choisir une année pour cibler des périodes précises. Ce premier graphe est un graphique en barres représentant le nombre d’articles par jour.",
+    "Ce tableau de bord est focalisé sur les mots clés. Abordons d’abord le graphique principal. Nous pouvons choisir une année pour cibler des périodes précises. Ce premier graphe est un graphique en barres représentant le nombre d’articles par jour.",
     html.Br(),
-    "Lorsque l’on clique sur une barre, nous obtenons deux graphiques supplémentaires. Un premier qui est également un graphique en barres représentant les 5 mots clés les plus utilisés à cette date. Puis un deuxième graphique en lignes (line chart). Celui représente le nombre de fois que les mots clés ont été utilisé lors de cette même année. Cela permet de se rendre compte si c’est un mot clé utilisé fréquemment ou non. Nous pouvons alors emmètre des hypothèses quant aux évènements qui ont engendrés cela. Par exemple, pour le 8/10/2020 (avec le plus petit fichier), nous obtenons des mots clés tels que « Mali », « otage », « Sophie » et « libération » et en analysant le graphique en lignes, nous pouvons conclure que Sophie a été libéré ce jour là.",
+    "Lorsque l’on clique sur une barre, nous obtenons deux graphiques supplémentaires. Un premier qui est également un graphique en barres représentant les 5 mots clés les plus utilisés à cette date. Puis un deuxième graphique en lignes (line chart). Celui-ci représente le nombre de fois que les mots clés ont été utilisé lors de cette même année. Cela permet de se rendre compte si c’est un mot clé utilisé fréquemment ou non. Nous pouvons alors émettre des hypothèses quant aux évènements qui ont engendrés cela. Par exemple, pour le 8/10/2020 (avec le fichier 'mali de fdesouche' ), nous obtenons des mots clés tels que « Mali », « otage », « Sophie » et « libération » et en analysant le graphique en lignes, nous pouvons conclure que Sophie a été libéré ce jour là.",
     html.Br(),
     "En outre, nous pouvons isoler un mot clé sur le deuxième graphique en cliquant sur la barre correspondant à celui-ci, ce qui permet d’observer plus facilement son évolution au cours de l’année.",
     ]
@@ -31,17 +31,17 @@ Keywords_text = [
 Organizations_text = [
     "Ce tableau de bord porte sur l'analyse des différentes organisations citées dans les articles de chaque fichier. Le premier graphique est un graphique en barre animé permettant de visualiser pour les organisations les plus citées, le nombre de fois qu’une de ces organisations est présente. Ceci est effectué pour chaque mois de chaque année.",
     html.Br(),
-    "Il est par ailleurs possible de cliquer sur une barre de ce graphique pour afficher de nouveaux résultats sur les deux autres. Un pie chart ainsi qu'un tableau s'affiche alors, le premier affiche les organisations les plus citées en même temps que l'organisation sélectionnée (10 organisations maximum). Ce graphique nous permet par exemple d'observer que les groupes de l'état islamique sont la plupart du temps citée les uns avec les autres et représente la plupart du temps à eux seul 50% des organisations présente lorsque l'une d'elle est citée",
+    "Il est par ailleurs possible de cliquer sur une barre de ce graphique pour afficher de nouveaux résultats sur les deux autres. Un diagramme circulaire ainsi qu'un tableau s'affichent alors, le premier affiche les organisations les plus citées en même temps que l'organisation sélectionnée (10 organisations maximum). Ce graphique nous permet, par exemple d'observer que les groupes de l'état islamique sont la plupart du temps cités les uns avec les autres et représentent la plupart du temps à eux seul 50% des organisations présentent lorsque l'une d'elles est citée.",
     html.Br(),
-    "Le tableau quant à lui affiche une description de l'organisation afin de pouvoir se renseigner rapidement sur celle-ci."
+    "Le tableau quant à lui, affiche une description de l'organisation afin de pouvoir se renseigner rapidement sur celle-ci."
 ]
 
 Conclusion_text = [
-    "A travers ce projet, nous avons appris à créer nos propres DashBoard. Cela est très important pour nous puisque nous serons menés à manipuler des données.",
+    "A travers ce projet, nous avons appris à créer nos propres Dashboard. Cela est très important pour nous puisque nous serons menés à manipuler des données.",
     html.Br(),
     "Nous pouvons alors nous rendre compte de l'importance des graphiques pour visualiser les données.",
     html.Br(),
-    "Enfin, voici un schéma de notre application avec les pages différentes ainsi que les callback.",
+    "Enfin, voici un schéma de notre application avec les différentes pages ainsi que les callback.",
     ]
 
 schema = './schema.png'
